fa12bcbc1e6dd37200a235de9b222c9b3586102a
marick2003 <marick2003@users.noreply.github.com>
2018-08-22 19:05:39 +0800

    add

--- 2018/index.php
+++ 2018/index.php
@@ -7,6 +7,106 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div id="wrap">
+         <div id="main">
+             <div id="list" class="box">
+             <div class="box-title">
+						<div class="box-top">
+							<div class="box-top-inner">
+								<div class="left beer">
+									<img class="bubble" src="images/index/index-img02.png">
+									<img class="pic" src="images/index/index-img01.png">
+									<div class='fly-bubble'></div>
+								</div>
+								<div class="right beer">
+									<img class="bubble" src="images/index/index-img04.png">
+									<img class="pic" src="images/index/index-img03.png">
+									<div class='fly-bubble'></div>
+								</div>
+								<div class="caption1"></div>
+								<img class="title" src="images/index/index-img06.png">
+								<img class="caption2" src="images/index/index-img07.png">
+								<img class="natural" src="images/index/index-img08.png">
+								<div class="desc">
+									<div class="line"><span class="black">從賈伯斯到披頭四，「車庫」成為孕育世界各種偉大創意的起點。</span></div>
+									<div class="line">在台灣，也有一群正為熱愛燃燒、為守護文化及土地閃耀的青年，</div>
+									<div class="line">這群用新思維闖出新局的歸鄉青年，我們稱之為<span class="black">「返巢世代」</span>。</div>
+									<div class="line">KIRIN Bar BEER 將以<span class="black">「創造共享價值(Creating Shared Value)」</span>，全心投入支持行動，為擁有<span class="black">車庫精神</span>的大家，加油打氣！</div>
+								</div>
+								<div class="godown">
+									<img class="arrow" src="images/index/index-img10.png">
+								</div>
+							</div>
+							<div class="box-top-inner shadow">
+								<div class="left beer">
+									<img class="bubble" src="images/index/index-img02.png">
+									<img class="pic" src="images/index/index-img01.png">
+									<div class='fly-bubble'></div>
+								</div>
+								<div class="right beer">
+									<img class="bubble" src="images/index/index-img04.png">
+									<img class="pic" src="images/index/index-img03.png">
+									<div class='fly-bubble'></div>
+								</div>
+								<div class="caption1"></div>
+								<img class="title" src="images/index/index-img06.png">
+								<img class="caption2" src="images/index/index-img07.png">
+								<img class="natural" src="images/index/index-img08.png">
+							</div>
+						</div>
+						<div class="bg">
+							<div class="bg2"></div>
+							<div class="bg1"></div>
+						</div>
+					</div>
+					<div class="box-content">
+						<div class="catalog">
+							<div class="inner">
+								<div class="group">
+									<div class="group-content">
+										<ul class="clearFix">
+											<li>
+												<div class="pop"></div>
+												<div class="inner">
+													<a href="inner-1.php">
+														<div class="pic"><img src="images/item/1/cover.jpg"></div>
+														<div class="info">
+															<p class="text">用創意重新詮釋在地傳統，讓文化永續這就是「甘」之如飴、「樂」在其中的社區再造之路</p>
+														</div>
+													</a>
+												</div>
+											</li>
+											<li>
+												<div class="pop"></div>
+												<div class="inner">
+													<a href="inner-2.php">
+														<div class="pic"><img src="images/item/2/cover.jpg"></div>
+														<div class="info">
+															<p class="text">一場激烈的社會運動後，這些青年是如何「掀」起各地青年返鄉的暖風呢？</p>
+														</div>
+													</a>
+												</div>
+											</li>
+											<li>
+												<div class="pop"></div>
+												<div class="inner">
+													<a href="inner-3.php">
+														<div class="pic"><img src="images/item/3/cover.jpg"></div>
+														<div class="info">
+															<p class="text">取材在地農糧的香Q饅頭，重拾部落經濟與對土地的尊重，就是他們奮鬥不懈的目標</p>
+														</div>
+													</a>
+												</div>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="bg"></div>
+					</div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
