bbdcc830da109888c96a44d0e9e7d1d84ab5b838
marick2003 <mmaarriicckk@gmail.com>
2018-08-30 01:53:33 +0800

    add

---
+++ m/2018/inner-1.php
@@ -0,0 +1,177 @@
+<?
+	// include('../../../../config.php');
+	 include('js/data.php');
+	$id = 1;
+
+	if (!$data[$id]['title']) {
+		header("Location:./");
+		exit;
+	}
+	
+	// include('../../../include/head.php');
+?>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=375, initial-scale=1.0">
+<meta name="description" content="<?=$data[$id]['shareDescription']?>">
+<meta name="keywords" content="KIRIN,台灣麒麟,歡笑,Bar,bar,beer,公益,甘樂文創,苑裡掀海風,八穀農糧手工作坊,甘樂,苑里,八穀,耀就來Bar,車庫精神,閃耀啟程,返巢世代,三峽,藍染,無毒,饅頭,孝念米,免費,抽獎,林峻丞,熱血,返鄉,返鄉青年">
+
+<meta property="og:site_name" content="<?=$data[$id]['shareTitle']?>" /> 
+<meta property="og:title" content="<?=$data[$id]['shareTitle']?>" /> 
+<meta property="og:description" content="<?=$data[$id]['shareDescription']?>" />
+<meta property="og:image" content="https://www.kirin.com.tw/barbeer/CSV/2017/images/item/<?=$id?>/metaimg.jpg?v=20170712" />
+<meta property="og:url" content="https://www.kirin.com.tw/barbeer/CSV/2017/inner-<?=$id?>.php" />
+<meta property="fb:app_id" content="1830556680498946">
+
+<title><?=$data[$id]['title']?></title>
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.5/css/swiper.css">
+<link rel="stylesheet" type="text/css" href="css/common.css">
+<link rel="stylesheet" type="text/css" href="css/index.css">
+<script>
+	var detailID = <?=$id?>,
+		shareTitle = "<?=$data[$id]['shareTitle']?>",
+		shareDescription = "<?=$data[$id]['shareDescription']?>",
+		gaMark = "<?=$data[$id]['gaMark']?>";
+</script>
+<!-- <script type="text/javascript" src="../../../../js/libs/swipe.js"></script> -->
+<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+<script type="text/javascript" src="js/index.js"></script>
+
+<!-- Facebook Pixel Code -->
+<!-- <script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1857929141121316'); // Insert your pixel ID here.
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1857929141121316&ev=PageView&noscript=1"/></noscript> -->
+<!-- DO NOT MODIFY -->
+<!-- End Facebook Pixel Code -->
+
+</head>
+
+<body id="news">
+	<?php
+		//include('../../../include/track.php');
+	?>
+	<div id="wrap">
+		<?php
+			//include('../../../include/header.php');
+		?>
+		<!-- <script src="//cdn.doublemax.net/js/rtid.js"></script>
+		<script src="//dmp.eland-tech.com/dmpreceiver/eland_tracker.js"></script> -->
+		<script>
+			clickforce_rtid("8269001");
+			ElandTracker.Track({'source':'CAP8269',
+			'trackType':'view',
+			'trackSubfolderDepth':3,
+			'targetType':'usual'
+			});
+		</script>
+		<div id="wrap2">
+			<div id="main" class="detail">
+				<div class="inner">
+					<div id="detail" class="box">
+						<div class="box-title">
+							<p class="title"><?=$data[$id]['title']?></p>
+							<p class="date"><?=$data[$id]['date']?></p>
+						</div>
+						<div class="box-content">
+							<div class="video">
+								<div id="video"></div>
+							</div>
+                            <!-- <div class="btn-active">抽獎活動已結束</div> -->
+                            <div class="fb_btn">
+                                <img src="images/index/fb_btn.png" alt="">
+                            </div>
+							<div id="article">
+								<?=$data[$id]['article']?>
+							</div>
+							<div class="links">
+								<?
+									if ( $id != 1 ) {
+										$prevId = $id-1;
+									} else {
+										$prevId = count($data);
+									}
+									if ( $id != count($data) ) {
+										$nextId = $id+1;
+									} else {
+										$nextId = 1;
+									}
+								?>
+								<a class="prev" href="https://www.kirin.com.tw/barbeer/CSV/2017/inner-<?=$prevId?>.php">上一篇</a>
+								<a class="next" href="https://www.kirin.com.tw/barbeer/CSV/2017/inner-<?=$nextId?>.php">下一篇</a>
+							</div>
+							
+							<a class="back" href="https://www.kirin.com.tw/barbeer/CSV/2018/">返回 BarBEER 車庫精神 翻轉世代 列表頁</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<?php
+				//include('../../../include/footer.php');
+			?>
+		</div>
+	</div>
+	<?php
+		//include('../../../include/menu.php');
+		//include('../../../include/rotate.php');
+	?>
+
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.9&appId=1830556680498946";
+	fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+
+	<script>
+		var tag = document.createElement('script');
+
+		tag.src = "https://www.youtube.com/iframe_api";
+		var firstScriptTag = document.getElementsByTagName('script')[0];
+		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+		var player,
+		    prevYoutubeData = [ 0, 0 ];
+		function onYouTubeIframeAPIReady() {
+			player = new YT.Player('video', {
+				height: '179',
+				width: '100%',
+				videoId: '<?=$data[$id]['video']?>',
+				playerVars: {
+					iv_load_policy: 0,
+					modestbranding: 1,
+					rel: 0,
+					showinfo: 0,
+					fs: 0
+				},
+				events: {
+					'onStateChange': onPlayerStateChange
+				}
+			});
+		}
+
+		function onPlayerStateChange(event) {
+			switch (event.data) {
+				case -1:
+					isYoutubeStart = true;
+					break;
+				case 1:
+					if ( ( prevYoutubeData[0] == -1 && prevYoutubeData[1] == 3 ) || ( prevYoutubeData[1] == 0 ) || ( player.getCurrentTime() >= 0 && player.getCurrentTime() <= 0.5 ) ) {
+						sendEvent('分頁_' + gaMark +'_影片', '點選分頁_' + gaMark +'_影片', 'CSV');
+					}
+					break;
+			}
+			prevYoutubeData[0] = prevYoutubeData[1];
+			prevYoutubeData[1] = event.data;
+		}
+	</script>
+</body>
+</html>
