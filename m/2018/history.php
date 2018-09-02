d90863c32027696a4a2928de813bc3d3b0eb38ad
marick2003 <mmaarriicckk@gmail.com>
2018-09-02 10:20:22 +0800

    add

---
+++ m/2018/history.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/common.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/history.css">
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script type="text/javascript" src="js/index.js"></script>
+</head>
+<body>
+    <div id="wrap">
+        <div id="main" class='detail'>
+            <div class="inner">
+				<div id="history-detail">
+                       <div class="title">
+                            歷年車庫精神
+                        </div>
+                    <div class="item">
+                        
+                        <div class="content">
+                            <div class="subTitle">2018車庫精神</div>
+                            <div class="line"></div>
+                            <div class="text">
+                                <br>
+                                「返鄉很難，但能長久待在故鄉更難！」身為核心人物的劉育育體會到，唯有集體力量的支持與找出新的農村產業模式，才能減少青年返鄉後又再度出走的情況。透過網路宣傳行銷、在地小旅行的結合，幫助友善耕作的農夫、店家獲得收入；透過刊物的採訪和販售，成為青年採訪跟美編的收入。從增加收入管道，化被動為主動，吸引更多人深耕苑裡。<br>
+                                <br>
+                            </div>
+                            
+                            <div class="seemore">了解更多</div>
+                            <div>
+                                <img src="images/history/history_img1.png" alt="">
+                            </div>
+                          
+                        </div>
+                    </div>
+                    <div class="item">
+                        
+                        <div class="content">
+                            <div class="subTitle">2017車庫精神</div>
+                            <div class="line"></div>
+                            <div class="text">
+                                <br>
+                                「返鄉很難，但能長久待在故鄉更難！」身為核心人物的劉育育體會到，唯有集體力量的支持與找出新的農村產業模式，才能減少青年返鄉後又再度出走的情況。透過網路宣傳行銷、在地小旅行的結合，幫助友善耕作的農夫、店家獲得收入；透過刊物的採訪和販售，成為青年採訪跟美編的收入。從增加收入管道，化被動為主動，吸引更多人深耕苑裡。<br>
+                                <br>
+                            </div>
+                            
+                            <div class="seemore">了解更多</div>
+                            <div>
+                                <img src="images/history/history_img2.png" alt="">
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="item">
+                        
+                        <div class="content">
+                            <div class="subTitle">2016車庫精神</div>
+                            <div class="line"></div>
+                            <div class="text">
+                                <br>
+                                「返鄉很難，但能長久待在故鄉更難！」身為核心人物的劉育育體會到，唯有集體力量的支持與找出新的農村產業模式，才能減少青年返鄉後又再度出走的情況。透過網路宣傳行銷、在地小旅行的結合，幫助友善耕作的農夫、店家獲得收入；透過刊物的採訪和販售，成為青年採訪跟美編的收入。從增加收入管道，化被動為主動，吸引更多人深耕苑裡。<br>
+                                <br>
+                            </div>
+                            
+                            <div class="seemore">了解更多</div>
+                            <div>
+                                <img src="images/history/history_img3.png" alt="">
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+                
+            </div>
+            <div class="bg">	
+							<div class="fixed_bg">	
+								<div class="bg_beer toleft">
+									<div class='fly-bubble'>
+										
+									</div>
+									<img src="images/index/bg_beer.png" alt="">
+								</div>
+								<div class="bg_beer toright">
+									<div class='fly-bubble'></div>
+								    <img src="images/index/bg_beer2.png" alt="">
+								</div>
+				</div>
+        </div>
+    </div>
+</body>
+</html>
